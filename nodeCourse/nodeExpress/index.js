ac5f46871ae124ae585a51ff902f0a5c7b8afb23
kolldavi <kolldavi@gmail.com>
2018-07-22 21:24:52 -0500

    add promotions

--- nodeCourse/nodeExpress/index.js
+++ nodeCourse/nodeExpress/index.js
@@ -4,7 +4,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const dishRouter = require('./routes/dishRouter');
-
+const promoRouter = require('./routes/promoRouter');
 const hostname = 'localhost';
 const port = 3000;
 
@@ -12,28 +12,10 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-// //next callback after /dishes:dishId for get request
-// app.get('/dishes:dishId', (req, res, next) => {
-// 	res.end(`Will send details for this dish${req.params.dishId}`);
-// });
-// //next callback after all for post request for /dishes:dishId
-// app.post('/dishes:dishId', (req, res, next) => {
-// 	//403 error is not supported
-// 	res.statusCode = 403;
-// 	res.end('POST operation not supported on /dishes:dishId');
-// });
-
-// app.put('/dishes:dishId', (req, res, next) => {
-// 	res.write(`Will UPDATE this dish for ${req.params.dishId} \n`);
-// 	res.end(`Will UPDATE this dish:${req.body.name} with details: ${req.body.desciption}`);
-// });
-
-// //Should make sure only certain users can do this request
-// app.delete('/dishes:dishId', (req, res, next) => {
-// 	res.end(`Deleting this dish ${req.params.dishId}`);
-// });
-app.use('/dishes', dishRouter);
 app.use(express.static(__dirname + '/public'));
+app.use('/dishes', dishRouter);
+
+app.use('/promotions', promoRouter);
 
 app.use((req, res, next) => {
 	console.log(req.headers);
--- nodeCourse/nodeExpress/routes/dishRouter.js
+++ nodeCourse/nodeExpress/routes/dishRouter.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const dishRouter = express.Router();
 dishRouter.use(bodyParser.json());
 //request for all get post put delete
-//set initial endpoint
+//set initial endpoint for /dishes
 dishRouter
 	.route('/')
 	.all((req, res, next) => {
@@ -25,6 +25,24 @@ dishRouter
 	})
 	.delete((req, res, next) => {
 		res.end('Deleting all this dishes');
+	});
+//set endpoint for /dishes:dishId
+dishRouter
+	.route('/:dishId')
+	.all((req, res, next) => {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/plain');
+		next();
+	})
+	.get((req, res, next) => {
+		res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
+	})
+	.put((req, res, next) => {
+		res.write('Updating the dish: ' + req.params.dishId + '\n');
+		res.end('Will update the dish: ' + req.body.name + ' with details: ' + req.body.description);
+	})
+	.delete((req, res, next) => {
+		res.end('Deleting dish: ' + req.params.dishId);
 	});
 
 module.exports = dishRouter;
