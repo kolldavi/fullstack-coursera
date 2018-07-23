9262c18a3bd5d4f75ad903f8ee0fb2b19b943e50
kolldavi <kolldavi@gmail.com>
2018-07-22 21:50:06 -0500

    add comments to project

--- nodeCourse/nodeExpress/index.js
+++ nodeCourse/nodeExpress/index.js
@@ -2,18 +2,20 @@ const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-
 const routes = require('./routes');
+
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
-
 app.use(express.static(__dirname + '/public'));
+
+// add all routes at once instead app.use('/dishes', dishRouter); app.use('/leaders', leadersRoutes); ...
 routes(app);
 
+//serve this page as default
 app.use((req, res, next) => {
 	console.log(req.headers);
 	res.statusCode = 200;
@@ -21,8 +23,8 @@ app.use((req, res, next) => {
 	res.end('<html><body><h1>This is an Express Server</h1></body></html>');
 });
 
+//start server
 const server = http.createServer(app);
-
 server.listen(port, hostname, () => {
 	console.log(`Server running at http://${hostname}:${port}`);
 });
