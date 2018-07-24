347ea078c3ffb8f251becf0322c5ad41aa06fc14
kolldavi <kolldavi@gmail.com>
2018-07-24 14:21:35 -0500

    broke up menu component into to functions

--- reactCourse/confusion/src/components/MenuComponent.js
+++ reactCourse/confusion/src/components/MenuComponent.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-function Menu(props) {
-	const dishes = props.dishes;
+function RenderCard(props) {
+	const { dish, onClick } = props;
+	return (
+		<Card onClick={() => onClick(dish.id)}>
+			<CardImg width="100%" src={dish.image} alt={dish.name} />
+			<CardImgOverlay>
+				<CardTitle>{dish.name}</CardTitle>
+			</CardImgOverlay>
+		</Card>
+	);
+}
+const Menu = ({ dishes, onClick }) => {
 	return (
 		<div className="container">
 			<div className="row">
 				{dishes.map(dish => {
 					return (
 						<div className="col-12 col-md-5 m-1" key={dish.id}>
-							<Card onClick={() => props.onClick(dish.id)}>
-								<CardImg width="100%" src={dish.image} alt={dish.name} />
-								<CardImgOverlay>
-									<CardTitle>{dish.name}</CardTitle>
-								</CardImgOverlay>
-							</Card>
+							<RenderCard dish={dish} onClick={onClick} />
 						</div>
 					);
 				})}
 			</div>
 		</div>
 	);
-}
+};
 export default Menu;
