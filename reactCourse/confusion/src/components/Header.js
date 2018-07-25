e0d74658a1b050f2c19b257307b28c739e04c713
kolldavi <kolldavi@gmail.com>
2018-07-25 15:06:25 -0500

    add react router to project

--- reactCourse/confusion/src/components/Header.js
+++ reactCourse/confusion/src/components/Header.js
@@ -1,21 +1,57 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand, Jumbotron } from 'reactstrap';
+import { Navbar, NavbarBrand, Jumbotron, Nav, NavbarToggler, NavItem, Collapse } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+
 class Header extends Component {
+	state = { isOpen: false };
+	toggleNav = () => {
+		this.setState(prev => {
+			return { isOpen: !prev.isOpen };
+		});
+	};
 	render() {
 		return (
 			<React.Fragment>
-				<Navbar dark>
+				<Navbar dark expand="md">
 					<div className="container">
-						<NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+						<NavbarToggler onClick={this.toggleNav} />
+						<NavbarBrand className="mr-auto" href="/">
+							<img src="assets/images/logo.png" height="30" width="41" alt="Ristorante Con Fusion" />
+						</NavbarBrand>
+						<Collapse navbar isOpen={this.state.isOpen}>
+							<Nav navbar>
+								<NavItem>
+									<NavLink className="nav-link" to="/home">
+										<span className="fa fa-home fa-lg" />Home
+									</NavLink>
+								</NavItem>
+								<NavItem>
+									<NavLink className="nav-link" to="/aboutus">
+										<span className="fa fa-info fa-lg" />About Us
+									</NavLink>
+								</NavItem>
+								<NavItem>
+									<NavLink className="nav-link" to="/menu">
+										<span className="fa fa-list fa-lg" />Menu
+									</NavLink>
+								</NavItem>
+								<NavItem>
+									<NavLink className="nav-link" to="/contactus">
+										<span className="fa fa-address-card fa-lg" />Contact Us
+									</NavLink>
+								</NavItem>
+							</Nav>
+						</Collapse>
 					</div>
 				</Navbar>
 				<Jumbotron>
 					<div className="container">
 						<div className="row row-header">
 							<div className="col-12 col-sm-6">
-								<h1>Restorante Con Fusion</h1>
+								<h1>Ristorante con Fusion</h1>
 								<p>
-									We take insperation from the World's best cuisines, and create a unique experience.
+									We take inspiration from the World's best cuisines, and create a unique fusion
+									experience. Our lipsmacking creations will tickle your culinary senses!
 								</p>
 							</div>
 						</div>
--- reactCourse/confusion/src/components/MainComponent.js
+++ reactCourse/confusion/src/components/MainComponent.js
@@ -4,7 +4,8 @@ import { DISHES } from '../shared/dishes';
 import Header from './Header';
 import Footer from './Footer';
 import DishDetail from './DishDetail';
-
+import Home from './Home';
+import { Switch, Route, Redirect } from 'react-router-dom';
 class Main extends Component {
 	constructor(props) {
 		super(props);
@@ -13,16 +14,20 @@ class Main extends Component {
 			selectedDish: null
 		};
 	}
-	onDishSelect(dishId) {
-		this.setState({ selectedDish: dishId });
-	}
 
 	render() {
 		return (
 			<div>
 				<Header />
-				<Menu dishes={this.state.dishes} onClick={dishId => this.onDishSelect(dishId)} />
-				<DishDetail dish={this.state.dishes.filter(dish => dish.id === this.state.selectedDish)[0]} />
+				<Switch>
+					<Route path="/home" component={Home} />
+					<Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+					<Redirect to="/home" />
+				</Switch>
+
+				{
+					//<DishDetail dish={this.state.dishes.filter(dish => dish.id === this.state.selectedDish)[0]} />
+				}
 				<Footer />
 			</div>
 		);
